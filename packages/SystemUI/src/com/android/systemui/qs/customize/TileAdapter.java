a5381658d0c1e62c70e70b61b553f45ba02d06e4
Andrzej Ressel <jereksel@gmail.com>
2017-08-09 00:11:16 +0200

    Allow user to add/remove QS with one click
    Demo: https://www.youtube.com/watch?v=SlnRKLa4K7o
    
    Change-Id: I9250fe5fd6a3edf567f1f4d30fd86838f5ef4610

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -323,6 +323,14 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         holder.mTileView.setFocusable(true);
         holder.mTileView.setFocusableInTouchMode(true);
 
+        if (mAccessibilityAction == ACTION_NONE) {
+            holder.mTileView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    move(holder.getAdapterPosition(), mEditIndex, holder.mTileView);
+                }
+            });
+        }
         if (mAccessibilityAction != ACTION_NONE) {
             holder.mTileView.setClickable(selectable);
             holder.mTileView.setFocusable(selectable);
