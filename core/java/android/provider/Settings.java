19d5c61acca9e667b189844250e37bfbfca60303
Ido Ben-Hur <idoybh2@gmail.com>
2019-10-17 04:15:00 +0300

    base: Add a toggle for QS footer text
    stx edit: squash toggle into SysUI tuner
    
    Change-Id: Ic987a96832df88bb6d23d728b7dc78796212f65a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4942,6 +4942,12 @@ public final class Settings {
         public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
 
         /**
+         * Whether footer text shows the build type
+         * @hide
+         */
+        public static final String FOOTER_TEXT_SHOW = "footer_text_show";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -5070,6 +5076,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
+            PRIVATE_SETTINGS.add(FOOTER_TEXT_SHOW);
         }
 
         /**
