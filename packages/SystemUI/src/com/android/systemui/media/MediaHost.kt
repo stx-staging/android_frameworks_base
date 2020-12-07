caf-ginkgo/frameworks_base

SystemUI: Make QS max rows customization more robust
This fixes 3 issues with the QS max rows customization:

1) On changing any SystemUI overlay such as accent or icon pack when media player is
   inactive, QS panel resets back to 3x2 size and stays there.

2) Similarly when media player is active and theme is altered, QS panel gets stuck
   at 3x3 with the media player at bottom, making it look ugly.

3) With "hide player when session has ended" disabled, when media player is closed, the
   player is visible but onMediaVisibilityChanged(false) gets triggered, causing QS
   panel to get stuck at 3x3 again.

packages/SystemUI/src/com/android/systemui/media/MediaHost.kt
@@ -112,11 +112,7 @@ class MediaHost @Inject constructor(
     }
 
     private fun updateViewVisibility() {
-        visible = if (showsOnlyActiveMedia) {
-            mediaDataFilter.hasActiveMedia()
-        } else {
-            mediaDataFilter.hasAnyMedia()
-        }
+        visible = mediaDataFilter.hasAnyMedia()
         val newVisibility = if (visible) View.VISIBLE else View.GONE
         if (newVisibility != hostView.visibility) {
             hostView.visibility = newVisibility

stx-staging/android_frameworks_base

Change-Id: I94b8fa1503019d034d285806fee95b2ef937beea

packages/SystemUI/src/com/android/systemui/media/MediaHost.kt
@@ -112,11 +112,7 @@ class MediaHost @Inject constructor(
     }
 
     private fun updateViewVisibility() {
-        visible = if (showsOnlyActiveMedia) {
-            mediaDataManager.hasActiveMedia()
-        } else {
-            mediaDataManager.hasAnyMedia()
-        }
+        visible = mediaDataManager.hasAnyMedia()
         val newVisibility = if (visible) View.VISIBLE else View.GONE
         if (newVisibility != hostView.visibility) {
             hostView.visibility = newVisibility